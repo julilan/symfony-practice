54e263ecea2c310fa27fdf074c540495d33f9137
julilan <juli.molnar@outlook.com>
2023-05-05 12:01:28 +0300

    add Fahrenheit to Celsius conversion

--- src/Controller/TemperatureController.php
+++ src/Controller/TemperatureController.php
@@ -1,19 +1,33 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TemperatureController {
-  
-  #[Route('/temp')]
-  public function tempConverter(Request $request) {
-    $temp = $request->query->get('temperature');
-    if(!is_numeric($temp)) {
-      return new Response("Error: Temperature must be a number", 400);
+class TemperatureController
+{
+
+    #[Route('/celsius-to-fahrenheit')]
+    public function celsiusToFahrenheit(Request $request)
+    {
+        $temp = $request->query->get('celsius');
+        if (!is_numeric($temp)) {
+            return new Response("Error: Temperature must be a number", 400);
+        }
+        $fahrenheit = ($temp * 9 / 5) + 32;
+        return new Response("$temp Celsius is $fahrenheit Fahrenheit");
     }
-    $fahrenheit = ($temp * 9 / 5) + 32;
-    return new Response("$temp Celcius is $fahrenheit Fahrenheit");
-  }
+
+    #[Route('/fahrenheit-to-celsius')]
+    public function fahrenheitToCelsius(Request $request)
+    {
+        $temp = $request->query->get('fahrenheit');
+        if (!is_numeric($temp)) {
+            return new Response("Error: Temperature must be a number", 400);
+        }
+        $celsius = ($temp - 32) * 5 / 9;
+        return new Response("$temp Fahrenheit is $celsius Celsius");
+    }
 }
